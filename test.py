52e6cf67a83ac466ce5b4cbc52ceafbab5b9ee44
Hagai Cohen <hagai.co@gmail.com>
2017-01-20 00:23:06 +0200

    chore(bugfixes): add unittest script

---
+++ test.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python
+from test.browser import *
+import unittest
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/browser.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python2
+import unittest
+
+__all__ = [ "TestBrowser" ]
+
+class TestBrowser(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+if __name__ == "__main__":
+    unittest.main()
